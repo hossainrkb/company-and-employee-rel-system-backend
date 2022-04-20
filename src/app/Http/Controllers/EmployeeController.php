a1f8c81e88f76f12ff6b557cd3d8913caedba31d
Rakib Hosen <rakib.151045@gmail.com>
2022-04-20 14:45:01 +0600

    add emp profile api

--- src/app/Http/Controllers/EmployeeController.php
+++ src/app/Http/Controllers/EmployeeController.php
@@ -6,6 +6,7 @@ use App\Models\Company;
 use App\Models\Employee;
 use Illuminate\Http\Request;
 use App\Traits\EmpDetailsTrait;
+use Illuminate\Support\Facades\Auth;
 
 class EmployeeController extends CentralController
 {
@@ -25,6 +26,29 @@ class EmployeeController extends CentralController
             return $this->empStat($companyId, $employeeId, $month, $year);
         } catch (\Throwable $th) {
             return error_response($th->getMessage());
+        }
+    }
+
+    public function getEmployee()
+    {
+        if (Auth::guard('employee_api')->user()) {
+            return response()->json([
+                'status' => 'ok',
+                'data' => Auth::guard('employee_api')->user()
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+            ]);
+        }
+    }
+    public function logout()
+    {
+        if (Auth::guard('employee_api')->user()) {
+            Auth::guard('employee_api')->user()->AauthAcessToken()->delete();
+            return true;
+        } else {
+            return null;
         }
     }
 }
